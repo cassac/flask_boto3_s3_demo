c6999df7b2712b0834f4d7fb0fd5398442613159
cassac <hicassac@gmail.com>
2016-05-04 15:33:23 -0700

    adds thumbnail url field to image class model

--- models.py
+++ models.py
@@ -8,11 +8,13 @@ class Image(Base):
     title = Column(String(50))
     filename = Column(String(50))
     url = Column(String(120), unique=True)
+    thumb_url = Column(String(120), unique=True)
 
-    def __init__(self, title, filename, url):
+    def __init__(self, title, filename, url, thumb_url):
         self.title = title
         self.filename = filename
         self.url = url
+        self.thumb_url = thumb_url
 
     def __repr__(self):
         return '<Image %r>' % (self.filename)
